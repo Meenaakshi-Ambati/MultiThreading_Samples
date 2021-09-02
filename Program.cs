a346cc4dd0f2006cbfc93954d09230e09b0200b2
Meenaakshi-Ambati <ambatimeenakshi9@gmail.com>
2021-09-02 08:53:05 +0530

    MultiThreading sample1

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+
+namespace MultiThreding_Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main started");
+
+           Thread t1 = new Thread(Program.Thread1Function);
+            t1.Start();
+
+            Thread t2 = new Thread(Program.Thread2Function);
+            t2.Start();
+
+             Console.WriteLine("Main completed");
+        }
+
+        public static void Thread1Function()
+        {
+            Console.WriteLine("Thread1Function started");
+        }
+
+       public static void Thread2Function()
+       {
+        Console.WriteLine("Thread2Function started");
+       }
+    }
+}
